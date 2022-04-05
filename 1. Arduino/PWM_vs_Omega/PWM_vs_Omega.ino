fde1590473ded989579322e745ea09097afdc209
thusaradimuth <thusaradimuthwanigathunga@gmail.com>
2022-04-05 10:41:35 +0530

    Update PWM vs Omega.ino with new coefficients.

--- 1. Arduino/PWM_vs_Omega/PWM_vs_Omega.ino
+++ 1. Arduino/PWM_vs_Omega/PWM_vs_Omega.ino
@@ -1,3 +1,5 @@
+float PWM_ex = 0;
+float Omega_ex = 0;
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,38 +7,39 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  PWM_ex = PWM(2.0,"L");
+  Omega_ex = Omega(100,"L");
 }
-float PWM(w,motor){
+float PWM(float w,char motor){
   float pwm;
   if (w == 0){
     pwm = 0; 
   }
   if (motor == "L"){
-    pwm = 2.54647926*pow(w,2) + 23.38200899*w + 26.15273771;
+    pwm = -1.9765702*pow(w,2) + 34.70570173*w + 24.1273073;
   }
   else{
-    pwm = 3.92203389*pow(w,2) + 12.97286726*w + 26.98219747;
+    pwm = -5.55509933*pow(w,2) + 43.87695327*w + 22.18873416;
   }
   return pwm;
 }
 
-float Omega(pwm,motor){
+float Omega(int pwm,char motor){
   float omega;
   if (motor == "L"){
-    if (pwm <= 22){
+    if (pwm <= 18){
       omega = 0;
     }
     else{
-      omega = -0.0000692232693*pow(pwm) + 0.0420306750*pwm + -1.01616927;
+      omega = -0.0000764041454*pow(pwm,2) + 0.0236812568*pwm - 0.596754982;
     }
   }
   if (motor == "R"){
-    if (pwm <= 23){
+    if (pwm <= 17){
       omega = 0;
     }
     else{
-      omega = -0.000141526413*pow(pwm) + 0.0570553215*pwm + -1.28250616;
+      omega = -0.000192967539*pow(pwm,2) + 0.00873435835*pwm - 0.246137506;
     }
   } 
 }
